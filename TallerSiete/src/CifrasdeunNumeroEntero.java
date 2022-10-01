34ef77a92b66e6def8f633970aa7c3dfbe8d4116
Estudiante USC <estudiante@GTACAD.USC>
2022-10-01 16:12:35 -0500

    TallerSiete

---
+++ TallerSiete/src/CifrasdeunNumeroEntero.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class CifrasdeunNumeroEntero {
+	static int numero = 0;
+	static int contador = 0;
+	static int  numTem =0;
+	static Scanner dig = new Scanner(System.in);
+	
+	public static void main(String args[]){
+	
+			
+		menu();
+		
+
+	}
+	
+	
+	public static void menu() {
+		
+		System.out.println("Digite un numero entero: ");
+		numero = dig.nextInt();
+		
+		cifras(numero);
+		
+	}
+		public static void cifras (int numero) {
+			
+			do {
+				numero  = numero/10;
+				
+				contador ++;
+				
+			} while (numero != 0);
+			
+			System.out.println("la cantidad de digitos es : " + contador);
+			contador = 0;
+			
+			
+			System.out.println("Deseas digitar otro numero : " + "\n" + " 1 si la respuesta es si " + "\n" + "2. si la respuestra es dos");
+			numTem = dig.nextInt();
+			
+			if (numTem == 1) {
+				
+				menu();
+				
+			}
+			else {
+				
+			}
+			
+		}
+		
+		public void finalizar() {
+			
+			System.out.println("el programa ha rterminado");
+		}
+		
+		
+	
+		
+	}
+
---
+++ TallerSiete/src/TallerSiete.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class TallerSiete {
+
+
+	public static void main(String args[]){
+		Scanner dig = new Scanner(System.in);
+		int anio = 0;
+		
+		System.out.println("Ingresa porfavor el año: ");
+		anio = dig.nextInt();
+		{
+		if ((anio % 4 == 0) && ((anio % 100!=0)|| (anio % 400 == 0))) {
+			System.out.println("El año es bisiesto");
+	    }else {
+				System.out.println("el año no es bisiesto");
+		
+	    }
+        }
+	}
+}
+	
+	
+	
